d1081198a8d47e8269dc198b26dffa3c3ae948c0
Ben de Graaff <ben@nx3d.org>
2016-02-16 17:19:22 +0100

    Add missing modules and complete xo-agent

--- host/post-setup.sh
+++ host/post-setup.sh
@@ -5,10 +5,9 @@ source config.sh
 (echo "${mirror}latest-stable/main";
  echo "${mirror}latest-stable/community") > ${chroot_dir}/etc/apk/repositories
 
-# TODO: replace with EC2-based config
 mkdir -p ${chroot_dir}/root/.ssh
 chmod 700 ${chroot_dir}/root/.ssh
-cat /home/ben/.ssh/id_ed25519.pub > ${chroot_dir}/root/.ssh/authorized_keys
+touch ${chroot_dir}/root/.ssh/authorized_keys
 
 rsync -av --no-owner --no-group overlay/ ${chroot_dir}/
 chroot ${chroot_dir} /bin/sh /root/bootstrap.sh
